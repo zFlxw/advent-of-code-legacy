cf55ac8b9ec16d639fde2763f8c7bfc15cb696bc
zFlxw <flxw@venterion.com>
2021-06-05 22:36:27 +0200

    Finished: 2015/04

---
+++ src/main/java/com/github/zflxw/adventofcode/modules/yearone/Day04.java
@@ -0,0 +1,23 @@
+package com.github.zflxw.adventofcode.modules.yearone;
+
+import com.github.zflxw.adventofcode.utils.AocModule;
+import com.github.zflxw.adventofcode.utils.Module;
+import org.apache.commons.codec.digest.DigestUtils;
+
+import java.util.List;
+
+@Module(year = "2015", day = "04")
+public class Day04 extends AocModule {
+    @Override
+    public void solution(String rawInput, List<String> input) {
+        for (int i = 0;; i++) {
+            String hash = DigestUtils.md5Hex(rawInput + i).toUpperCase();
+            // Replace with five / six zeros
+            if (hash.startsWith("000000")) {
+                System.out.println("Found matching hash! " + i);
+                System.out.println("Hash: " + hash);
+                break;
+            }
+        }
+    }
+}
