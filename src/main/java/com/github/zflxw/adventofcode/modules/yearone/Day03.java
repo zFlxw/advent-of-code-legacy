9cfa7e214acc93bb9dc1c64f25f89287fafa94eb
zFlxw <flxw@venterion.com>
2021-06-05 21:24:09 +0200

    Finished: 2015/03

--- src/main/java/com/github/zflxw/adventofcode/modules/yearone/Day03.java
+++ src/main/java/com/github/zflxw/adventofcode/modules/yearone/Day03.java
@@ -9,6 +9,14 @@ import java.util.List;
 public class Day03 extends AocModule {
     @Override
     public void solution(String rawInput, List<String> input) {
+        // Problem 01
+        problem01(rawInput);
+
+        // Problem 02
+        problem02(rawInput);
+    }
+
+    private void problem01(String rawInput) {
         int maxLength = rawInput.length();
         int[][] houses = new int[maxLength][maxLength];
 
@@ -42,6 +50,78 @@ public class Day03 extends AocModule {
                     houses[x][y]++;
                 }
             }
+        }
+
+        for (int i = 0; i < houses.length; i++)
+            for (int j = 0; j < houses[i].length; j++)
+                if (houses[i][j] >= 1)
+                    luckyHouses++;
+
+        System.out.println("Lucky Houses: " + luckyHouses);
+    }
+
+    private void problem02(String rawInput) {
+        int maxLength = rawInput.length() + 1;
+        // int maxLength = rawInput.length();
+        int[][] houses = new int[maxLength][maxLength];
+
+        int santaX = maxLength / 2;
+        int santaY = maxLength / 2;
+        int roboX = maxLength / 2;
+        int roboY = maxLength / 2;
+
+        int luckyHouses = 0;
+        int turns = 0;
+
+        houses[santaX][santaY] = 2;
+
+        for (char c : rawInput.toCharArray()) {
+            boolean santaTurn = turns % 2 == 0;
+
+            switch (c) {
+                // Go up
+                case '^' -> {
+                    if (santaTurn) {
+                        santaY++;
+                        houses[santaX][santaY]++;
+                    } else {
+                        roboY++;
+                        houses[roboX][roboY]++;
+                    }
+                }
+                // Go down
+                case 'v' -> {
+                    if (santaTurn) {
+                        santaY--;
+                        houses[santaX][santaY]++;
+                    } else {
+                        roboY--;
+                        houses[roboX][roboY]++;
+                    }
+                }
+                // Go left
+                case '<' -> {
+                    if (santaTurn) {
+                        santaX--;
+                        houses[santaX][santaY]++;
+                    } else {
+                        roboX--;
+                        houses[roboX][roboY]++;
+                    }
+                }
+                // Go right
+                case '>' -> {
+                    if (santaTurn) {
+                        santaX++;
+                        houses[santaX][santaY]++;
+                    } else {
+                        roboX++;
+                        houses[roboX][roboY]++;
+                    }
+                }
+            }
+
+            turns++;
         }
 
         for (int i = 0; i < houses.length; i++)
