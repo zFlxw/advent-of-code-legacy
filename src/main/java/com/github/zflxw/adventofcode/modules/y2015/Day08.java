b9bc781c336b8d47a0a486b1b8f18c8eb767eb9d
zFlxw <lxcalflxw@gmail.com>
2023-07-08 17:24:17 +0200

    feat: day08 part 1

--- src/main/java/com/github/zflxw/adventofcode/modules/y2015/Day08.java
+++ src/main/java/com/github/zflxw/adventofcode/modules/y2015/Day08.java
@@ -3,45 +3,30 @@ package com.github.zflxw.adventofcode.modules.y2015;
 import com.github.zflxw.adventofcode.utils.AocModule;
 import com.github.zflxw.adventofcode.utils.Module;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Module(year = "2015", day = "08")
 public class Day08 extends AocModule {
     @Override
     public void solution(String rawInput, List<String> input) {
-        long totalLength = 0;
         long codeLength = 0;
+        long inMemoryLength = 0;
 
         for (String string : input) {
-            System.out.println(string);
-            totalLength += string.length();
-            String code = string.substring(1, string.length() - 1)
-                    .replace("\\\\", "\\")
-                    .replace("\\\"", "\"");
-            System.out.println(code);
+            codeLength += string.length();
+            String content = string.substring(1, string.length() - 1)
+                    .replaceAll("\\\\", "@")
+                    .replaceAll("@\\\"", "!")
+                    .replaceAll("@@", "#")
+                    .replaceAll("@x..", "%");
 
-            if (code.contains("\\x")) {
-                char numOne = code.charAt(code.indexOf("\\x") + 2);
-                char numTwo = code.charAt(code.indexOf("\\x") + 3);
-
-                if (this.isValidHex(numOne) && this.isValidHex(numTwo)) {
-                    int character = Integer.parseInt(code.substring(code.indexOf("\\x") + 2, code.indexOf("\\x") + 4), 16);
-                    char c = (char) character;
-                    System.out.println("\\" + numOne + numTwo + " = " + character + " = " + c);
-                    code = code.replace("\\x" + numOne + numTwo, String.valueOf(c));
-                    System.out.println(code);
-                }
-            }
-
-            System.out.println(code.length());
-            codeLength += code.length();
-            System.out.println("---");
-
+            inMemoryLength += content.length();
         }
 
-        System.out.println("Total Length: " + totalLength);
         System.out.println("Code Length: " + codeLength);
-        System.out.println("Difference: " + (totalLength - codeLength));
+        System.out.println("In Memory Length: " + inMemoryLength);
+        System.out.println("Difference: " + (codeLength - inMemoryLength));
     }
 
     private boolean isValidHex(char c) {
