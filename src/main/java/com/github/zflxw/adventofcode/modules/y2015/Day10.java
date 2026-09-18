f022f922bb973a7a47be673c26f4c72b92fe6b63
Maik Zimmerschied <zimmerschied@sportmed.eu>
2026-09-18 16:09:09 +0200

    feat: day10 primitive

---
+++ src/main/java/com/github/zflxw/adventofcode/modules/y2015/Day10.java
@@ -0,0 +1,50 @@
+package com.github.zflxw.adventofcode.modules.y2015;
+
+import com.github.zflxw.adventofcode.utils.AocModule;
+import com.github.zflxw.adventofcode.utils.Module;
+
+import java.util.List;
+
+@Module(year = "2015", day = "10")
+public class Day10 extends AocModule {
+    private final int N = 50;
+
+    @Override
+    public void solution(String rawInput, List<String> input) {
+        String last = rawInput;
+        for (int i = 0; i < N; i++) {
+            System.out.println("Current Interation: " + i);
+            last = lookAndSee(last);
+        }
+
+        System.out.println("Look-and-see (Length): " + last.length());
+    }
+
+    private String lookAndSee(String input) {
+        StringBuilder out = new StringBuilder();
+        int n = 0;
+        char last = 0;
+        for (int i = 0; i < input.toCharArray().length; i++) {
+            char c = input.toCharArray()[i];
+            if (last == 0) {
+                last = c;
+                n = 1;
+                continue;
+            }
+
+            if (c == last) {
+                n++;
+                continue;
+            }
+
+            out.append(n).append(last);
+            n = 1;
+            last = c;
+        }
+
+        out.append(n).append(last);
+
+        return out.toString();
+    }
+}
+
